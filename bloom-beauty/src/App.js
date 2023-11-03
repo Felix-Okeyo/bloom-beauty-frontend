aec532e8521e3495415437d17262f951897ee50e
Crystal <muthonigachanja2000@gmail.com>
2023-11-03 09:40:19 -0700

    Adding the Payment file

--- bloom-beauty/src/App.js
+++ bloom-beauty/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Product from './components/Product';
 import Checkout from './components/Checkout'; 
+import Payment from './components/Payment';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Product />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/payment" component={<Payment />} />
       </Routes>
     </Router>
   );
--- bloom-beauty/src/components/Product.js
+++ bloom-beauty/src/components/Product.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Product() {
   return (
-    <section class="py-12 sm:py-16"> 
-  <div class="container mx-auto px-4">
-    <nav class="flex">
-      <ol role="list" class="flex items-center">
+    <section className="py-12 sm:py-16"> 
+  <div className="container mx-auto px-4">
+    <nav className="flex">
+      <ol role="list" className="flex items-center">
         <li class="text-left">
           <div class="-m-1">
             <a href="#" class="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"> Home </a>
@@ -32,8 +32,8 @@ function Product() {
       </ol>
     </nav>
 
-    <div class="lg:col-gap-12 xl:col-gap-16 mt-8 grid grid-cols-1 gap-12 lg:mt-12 lg:grid-cols-5 lg:gap-16">
-      <div class="lg:col-span-3 lg:row-end-1">
+    <div className="lg:col-gap-12 xl:col-gap-16 mt-8 grid grid-cols-1 gap-12 lg:mt-12 lg:grid-cols-5 lg:gap-16">
+      <div className="lg:col-span-3 lg:row-end-1">
         <div class="lg:flex lg:items-start">
           <div class="lg:order-2 lg:ml-5">
             <div class="max-w-xl overflow-hidden rounded-lg">
@@ -57,7 +57,7 @@ function Product() {
         </div>
       </div>
 
-      <div class="lg:col-span-2 lg:row-span-2 lg:row-end-2">
+      <div className="lg:col-span-2 lg:row-span-2 lg:row-end-2">
         <h1 class="sm: text-2xl font-bold text-gray-900 sm:text-3xl">Afro-Brazillian Coffee</h1>
 
         <div class="mt-5 flex items-center">
@@ -115,39 +115,39 @@ function Product() {
           </label>
         </div>
 
-        <div class="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
+        <div className="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
           <div class="flex items-end">
             <h1 class="text-3xl font-bold">$60.50</h1>
             <span class="text-base">/month</span>
           </div>
 
           <button type="button" class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
-            <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
             Add to cart
           </button>
           </div>
         </div>
 
-        <ul class="mt-8 space-y-2">
+        <ul className="mt-8 space-y-2">
           <li class="flex items-center text-left text-sm font-medium text-gray-600">
             <svg class="mr-2 block h-5 w-5 align-middle text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" class=""></path>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" class=""></path>
             </svg>
             Free shipping worldwide
           </li>
 
           <li class="flex items-center text-left text-sm font-medium text-gray-600">
             <svg class="mr-2 block h-5 w-5 align-middle text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" class=""></path>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" class=""></path>
             </svg>
             Cancel Anytime
           </li>
         </ul>
       </div>
 
-        <div class="mt-8 flow-root sm:mt-12">
+        <div className="mt-8 flow-root sm:mt-12">
           <h1 class="text-3xl font-bold">Delivered To Your Door</h1>
           <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusantium nesciunt fuga.</p>
           <h1 class="mt-8 text-3xl font-bold">From the Fine Farms of Brazil</h1>
